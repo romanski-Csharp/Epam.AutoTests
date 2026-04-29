ee19e27307d7fd1fbc7e2ee8173de897efde78cd
Роман Чуб <Tonik7star@gmail.com>
2026-04-30 01:00:35 +0300

    Add headless and stability args to Chromium driver options
    Added Chrome arguments for headless mode, window size, and improved stability (no-sandbox, disable-dev-shm-usage, disable-gpu) in ConfigureDownloadOptions to enhance automated environment compatibility.

--- Core/DriverFactory/ChromiumDriverFactory.cs
+++ Core/DriverFactory/ChromiumDriverFactory.cs
@@ -12,6 +12,12 @@ namespace Core.DriverFactory
             options.AddUserProfilePreference("download.default_directory", downloadDirectory);
             options.AddUserProfilePreference("download.prompt_for_download", ConfigManager.Instance.PromptForDownload);
             options.AddUserProfilePreference("plugins.always_open_pdf_externally", ConfigManager.Instance.AlwaysOpenPdfExternally);
+
+            options.AddArgument("--window-size=1920,1080");
+            options.AddArgument("--headless=new");
+            options.AddArgument("--no-sandbox");
+            options.AddArgument("--disable-dev-shm-usage");
+            options.AddArgument("--disable-gpu");
         }
     }
 }
